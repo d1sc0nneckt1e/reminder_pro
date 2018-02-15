f0ef57a6c779bd3ccb491c824096febc8cb08465
d1sc0nneckt1e <a.c.rezai@gmail.com>
2018-02-15 15:37:01 +0100

    mapping dispatch to props and binding action creators

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import { addReminder } from '../actions';
 import '../App.css';
 
@@ -13,8 +12,7 @@ class App extends Component {
   }
 
   addReminder(){
-    console.log('this', this);
-
+    this.props.addReminder(this.state.text);
   }
 
   render(){
@@ -32,8 +30,4 @@ class App extends Component {
   }
 }
 
-function mapDispatchToProps(dispatch){
-  return bindActionCreators({addReminder}, dispatch);
-}
-
-export default connect(null, mapDispatchToProps)(App);
+export default connect(null, { addReminder })(App);
